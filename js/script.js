c81d646da8f79c77c96c4e2ed911e1f9b3ca5889
Diego Reis <diegoreso@me.com>
2023-04-14 20:35:02 -0300

    cpf validation

--- js/script.js
+++ js/script.js
@@ -20,7 +20,54 @@ form_section.addEventListener('submit', function(e){
   const radio_female = document.querySelector('#female')
 
 
-  if(!inputName.value || !inputEmail.value || !inputCpf.value || !check_gender.value){
+  const cpf_validation = function VerificaCPF(strCpf) {
+
+    var soma;
+    var resto;
+    soma = 0;
+    if (strCpf == "00000000000") {
+        return false;
+    }
+    
+    for (i = 1; i <= 9; i++) {
+        soma = soma + parseInt(strCpf.substring(i - 1, i)) * (11 - i);
+    }
+    
+    resto = soma % 11;
+    
+    if (resto == 10 || resto == 11 || resto < 2) {
+        resto = 0;
+    } else {
+        resto = 11 - resto;
+    }
+    
+    if (resto != parseInt(strCpf.substring(9, 10))) {
+        return false;
+    }
+    
+    soma = 0;
+    
+    for (i = 1; i <= 10; i++) {
+        soma = soma + parseInt(strCpf.substring(i - 1, i)) * (12 - i);
+    }
+    resto = soma % 11;
+    
+    if (resto == 10 || resto == 11 || resto < 2) {
+        resto = 0;
+    } else {
+        resto = 11 - resto;
+    }
+    
+    if (resto != parseInt(strCpf.substring(10, 11))) {
+        return false;
+    }
+    
+    return true;
+    }
+
+    
+
+  if(!inputName.value || !inputEmail.value || cpf_validation(inputCpf.value) == false || !check_gender.value){
 
     btn_form.classList.remove('button-done')
     btn_form.textContent = 'Enviar'
